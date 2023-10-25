641c538e77b2f202a226ff7d42477145c7d454b6
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-25 21:29:53 +0300

    update styles

---
+++ src/components/styles.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled('div')`
+  padding: 80px 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
